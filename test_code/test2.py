a09f9907ec1e03a4ac771616a5baa560114d4bd1
toriitorippy <toriikatsuya0707@gmail.com>
2021-05-15 23:53:04 +0900

    testcode

---
+++ test_code/test2.py
@@ -0,0 +1,43 @@
+
+import sys
+from io import StringIO
+import unittest
+
+
+class TestClass(unittest.TestCase):
+    def assertIO(self, input, output):
+        stdout, stdin = sys.stdout, sys.stdin
+        sys.stdout, sys.stdin = StringIO(), StringIO(input)
+        resolve()
+        sys.stdout.seek(0)
+        out = sys.stdout.read()[:-1]
+        sys.stdout, sys.stdin = stdout, stdin
+        self.assertEqual(out, output)
+
+    def test_入力例_1(self):
+        input = """2 2 4"""
+        output = """Yes"""
+        self.assertIO(input, output)
+
+    def test_入力例_2(self):
+        input = """10 10 10"""
+        output = """No"""
+        self.assertIO(input, output)
+
+    def test_入力例_3(self):
+        input = """3 4 5"""
+        output = """No"""
+        self.assertIO(input, output)
+
+def resolve():
+    A, B, C = list(map(int,input().split()))
+    #A, B, C = a
+    print("a")
+    if A**2 + B**2 < C**2:
+        print("Yes")
+    else:
+        print("No")
+
+
+if __name__ == "__main__":
+    unittest.main()
